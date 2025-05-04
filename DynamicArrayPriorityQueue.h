34e21ba42c838bac3785e468fe8bafda6811b4a1
Hleb Lizhbanau <gleblizhbanov@gmail.com>
2025-05-04 20:57:03 +0200

    Rename ArrayPriorityQueue class

---
+++ DynamicArrayPriorityQueue.h
@@ -0,0 +1,16 @@
+#ifndef PROJECT2_DYNAMICARRAYPRIORITYQUEUE_H
+#define PROJECT2_DYNAMICARRAYPRIORITYQUEUE_H
+
+#include "IPriorityQueue.h"
+
+namespace DataStructures
+{
+
+    class DynamicArrayPriorityQueue : public IPriorityQueue
+    {
+
+    };
+
+} // DataStructures
+
+#endif //PROJECT2_DYNAMICARRAYPRIORITYQUEUE_H
