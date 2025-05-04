6859d194b59870bb66227ed98d7e099865e39c69
Hleb Lizhbanau <gleblizhbanov@gmail.com>
2025-05-05 00:32:56 +0200

    Add QueueItem class

---
+++ QueueItem.h
@@ -0,0 +1,18 @@
+//
+// Created by glebl on 5/4/2025.
+//
+
+#ifndef PROJECT2_QUEUEITEM_H
+#define PROJECT2_QUEUEITEM_H
+
+namespace DataStructures
+{
+    template<typename E, typename P>
+    struct QueueItem
+    {
+        E element;
+        P priority;
+    };
+}
+
+#endif //PROJECT2_QUEUEITEM_H
